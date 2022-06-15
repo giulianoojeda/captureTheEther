0750d0c99d6139f2ea621796b8699470952c2026
Giuliano Ojeda <giuliano@mokensleague.com>
2022-06-15 19:58:27 -0400

    finished warmup

---
+++ warmup/scripts/chooseNickname.ts
@@ -0,0 +1,5 @@
+import Web3 from 'web3';
+//ascii to hex web 3
+
+console.log((Web3.utils.asciiToHex('gioojeda.eth').padEnd(64, '0')));
+//0x67696f6f6a6564612e65746800000000000000000000000000000000000000
